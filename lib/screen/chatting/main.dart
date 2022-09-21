baf32506ea04eeb7e612ca60c6b05559cb37afbc
DaruNugroho <darunugroho00@gmail.com>
2022-09-21 19:38:20 +0700

    update chatting screen : menambahkan bubble chat

--- lib/screen/chatting/main.dart
+++ lib/screen/chatting/main.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:flutter/material.dart';
 
 class ChattingScreen extends StatelessWidget {
@@ -8,25 +8,53 @@ class ChattingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: <Widget>[
-              CircleAvatar(
-
-              ),
-              SizedBox(width: 8.0,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Faza", style: TextStyle(fontSize: 14.0),),
-                  Text("11.59",style: TextStyle(fontSize: 10.0),)
-                ],
-              )
-            ],
-          ),
-         actions: [Icon(Icons.videocam), SizedBox(width: 8.0,), Icon(Icons.call), SizedBox(width: 8.0,), Icon(Icons.format_list_bulleted)],
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            CircleAvatar(),
+            SizedBox(
+              width: 8.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                Text(
+                  "Faza",
+                  style: TextStyle(fontSize: 14.0),
+                ),
+                Text(
+                  "11.59",
+                  style: TextStyle(fontSize: 10.0),
+                )
+              ],
+            )
+          ],
         ),
-      body: Center(child: Text("Under Constructions"),),
+        actions: [
+          Icon(Icons.videocam),
+          SizedBox(
+            width: 8.0,
+          ),
+          Icon(Icons.call),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  BubbleSpecialOne(text: "TESTING", seen: true, isSender: true,),
+                ],
+              ),
+            ),
+            flex: 7,
+          ),
+          Expanded(
+            child: MessageBar(),
+          )
+        ],
+      ),
     );
   }
 }
